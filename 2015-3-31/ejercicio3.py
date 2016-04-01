07b7f8dc6b54b35ae7b9333b4ba377a68e131ab6
jgodoyc <jgodoyc@hotmail.com>
2016-04-01 02:14:46 +0000

    ejercicio 3 sin mucho avance

--- 2015-3-31/ejercicio3.py
+++ 2015-3-31/ejercicio3.py
@@ -22,15 +22,26 @@ class nota():
 		return self.modificar
 
 class alumno(persona):
-	def __init__(self, correo, carrera, promocion, notas):
+	def __init__(self, rut, nombre, nacimiento, sexo, correo, carrera, promocion, notas):
+		self.rut = rut
+		self.nombre = nombre
+		self.nacimiento = nacimiento
+		self.sexo = sexo
 		self.correo = correo
 		self.carrera = carrera
 		self.promocion = promocion
-		self.notas = notas
-	def agregar_nota(self):
+		self.notas = []
+	def agregar_nota(self, notas):
 		return self.notas
-	def PGA(self):
+	def PGA(self, prom):
+		for i in self.notas[]:
+			promg += self.promedio_ramo
+		promg = promg/len(self.notas[])
 		return self.prom_general
-	def promedio_ramo(self, ):
+	def promedio_ramo(self, notas, ramo):
 		return self.promedio_ramo
 
+if __name__ == "__main__":
+	alumno1 = alumno(18283788, "javier", "22 abril", "H", "jgodoyc@hotmail.com", 1, 2014, [5.0, 6.1,3.6, 4.5]):
+
+	
