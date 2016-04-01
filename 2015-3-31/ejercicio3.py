86bc2b67d16f69d90018044ee4f7b2a86d4a837d
jgodoyc <jgodoyc@hotmail.com>
2016-04-01 02:27:47 +0000

    ejercicio 3 NO terminado

--- 2015-3-31/ejercicio3.py
+++ 2015-3-31/ejercicio3.py
@@ -34,14 +34,13 @@ class alumno(persona):
 	def agregar_nota(self, notas):
 		return self.notas
 	def PGA(self, prom):
-		for i in self.notas[]:
+		for i in self.notas:
 			promg += self.promedio_ramo
-		promg = promg/len(self.notas[])
+		promg = promg/len(self.notas)
 		return self.prom_general
 	def promedio_ramo(self, notas, ramo):
 		return self.promedio_ramo
 
 if __name__ == "__main__":
-	alumno1 = alumno(18283788, "javier", "22 abril", "H", "jgodoyc@hotmail.com", 1, 2014, [5.0, 6.1,3.6, 4.5]):
-
+	#resto de codigo que no hice :l
 	
